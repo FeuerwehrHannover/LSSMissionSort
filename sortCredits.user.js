84165727df690f133635bbf4807f34f9e1f62134
FeuerwehrHannover <46085763+FeuerwehrHannover@users.noreply.github.com>
2018-12-23 14:52:43 +0100

    Create sortCredits.user.js

---
+++ sortCredits.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         AUTOUPDATE | Einsatzsortierung
+// @version      0.1
+// @description  Einsätze sortieren lassen
+// @author       FeuerwehrHannover
+// @match        https://www.leitstellenspiel.de/
+// @grant        none
+// ==/UserScript==
+$.getScript('https://cdn.jsdelivr.net/gh/FeuerwehrHannover/LSSMissionSort/sortCredits.js', function(){}, true);
