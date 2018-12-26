e79e7d1e47a187d7b77b4bb3f1c1d5583096718f
FeuerwehrHannover <46085763+FeuerwehrHannover@users.noreply.github.com>
2018-12-26 18:59:35 +0100

    Create sortDate.user.js

---
+++ sortDate.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         AUTOUPDATE | Einsatzsortierung Datum
+// @version      0.1
+// @description  Einsätze nach dem Erstellungsdatum sortieren lassen
+// @author       FeuerwehrHannover
+// @match        https://www.leitstellenspiel.de/
+// @grant        none
+// ==/UserScript==
+$.getScript('https://cdn.jsdelivr.net/gh/FeuerwehrHannover/LSSMissionSort/Date.js', function(){}, false);
